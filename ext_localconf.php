e31695d0e4d98b1af3bd556650a3877415344c28
Aristeidis Karavas <aristeidis.karavas@ifabrik.de>
2019-11-05 16:46:30 +0100

    feat: TYPO3 10 support

--- ext_localconf.php
+++ ext_localconf.php
@@ -8,4 +8,4 @@ defined('TYPO3_MODE') || die ('Access denied.');
     <INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TSConfig/Page.typoscript">
 ');
 // Register for hook to show preview of tt_content element of CType="highlight_js" in page module
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['highlight_js'] = \Vendor\HighlightJs\Hooks\PageLayoutView\HighlightPreviewRenderer::class;
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['tt_content_drawItem']['highlight_js'] = \Karavas\HighlightJs\Hooks\PageLayoutView\HighlightPreviewRenderer::class;
